c5fd9827165d2d0d07ee5cd98fc707b80dd70a7c
nguyenhoangphuongthao25042004-star <nguyenhoangphuongthao.25042004@gmail.com>
2025-12-12 15:24:44 +0700

    Lan thu tu

--- QLKho-backend/src/config/db.js
+++ QLKho-backend/src/config/db.js
@@ -7,9 +7,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,     // ← QUAN TRỌNG!
+    port: process.env.DB_PORT,
     dialect: 'mysql',
-    logging: false
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
